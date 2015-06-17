f1b9964f993722b788b3d36abc63b628358a22b1
Dominik <dominikzab@gmail.com>
2015-06-17 21:47:46 +0200

    Update postgres database in settings.py

--- felis/settings.py
+++ felis/settings.py
@@ -84,8 +84,11 @@ WSGI_APPLICATION = 'felis.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_name',                      
+        'USER': 'db_user',
+        'PASSWORD': 'db_user_password',
+        'HOST': ''
     }
 }
 

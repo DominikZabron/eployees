0c331253e99a6da278d7150a20b324f1d371887b
Dominik <dominikzab@gmail.com>
2015-08-14 00:27:10 +0200

    Fix

--- pracownicy/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Pracownik(models.Model):
-    fname_text = models.CharField(max_length=200)
-    lname_text = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.lname_text

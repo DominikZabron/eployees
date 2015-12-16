b233d02a30f471f95ac223b20774dfa3921947c3
DominikZabron <dominik.zabron@gmail.com>
2015-12-16 22:36:33 +0100

    next_inspection_date for cars, login to logout in header

--- assets/admin.py
+++ assets/admin.py
@@ -4,4 +4,5 @@ from .models import Car
 
 @admin.register(Car)
 class AdminCar(admin.ModelAdmin):
-	list_display = ('name', 'brand', 'registration_number')
+	list_display = ('name', 'brand', 'is_company_owned','next_tech_inspection',
+		'user',	'registration_number')
--- assets/models.py
+++ assets/models.py
@@ -1,3 +1,5 @@
+#-*- coding: utf-8 -*-
+
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -7,6 +9,7 @@ class Car(models.Model):
 	registration_number = models.CharField('Numer rejestracyjny', max_length=200)
 	engine_capacity = models.IntegerField('Pojemnosc silnika')
 	is_company_owned = models.BooleanField('Wlasnosc firmy')
+	next_tech_inspection = models.DateField('Przegląd ważny do', null=True, blank=True)
 	user = models.ForeignKey(User, verbose_name='Pracownik', null=True, blank=True)
 	
 	def __str__(self):
